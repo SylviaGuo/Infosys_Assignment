e47df2626c3dda788e82dcab5742e86af993b880
Sylvia <sylviaguo31@gmail.com>
2021-04-16 16:34:49 +1200

    feat:Generate pins

---
+++ typescript-react-example/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import GeneratePins from '../src/components/generatePins'
+
+function App() {
+  return (
+    <div className="App">
+      <GeneratePins></GeneratePins>
+    </div>
+  );
+}
+
+export default App;
---
+++ typescript-react-example/src/components/generatePins.tsx
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from 'react';
+
+interface pinNode {
+    pins: string[]
+}
+const GeneratePins: React.FC = () =>{
+    const [pinCollection, setPinCollection] = useState<pinNode>({pins:[]})
+
+    useEffect(()=>{
+        const pins = pinsGeneration(5)
+        setPinCollection({...pinCollection,pins})
+        console.log(pinCollection.pins)
+    }, [])
+
+    const pinsGeneration = (amount:number) => {
+        let pinsStr:Array<string> = []
+        let flag = 0
+        while(flag<amount){
+            const currentPin = singlePin()
+            if(isUnique(pinsStr,currentPin)){
+                pinsStr.push(currentPin)  
+                flag ++
+            }
+                
+        }
+        return pinsStr
+    }
+
+    const singlePin = () => {
+        const digits:Array<number> = []
+        while(digits.length < 4){
+            const number = Math.floor(Math.random()*10)
+            if(!isValidDigit(digits, number))
+                digits.push(number)
+        }     
+        return pinString(digits)
+    }
+
+    const isValidDigit = (digits: Array<number>, current: number) => {
+        if (digits.length !== 0){
+            //Check if the last number in the array is the same as the current number
+            if(digits[digits.length-1] === current)
+                return true
+            //check if digits are ascending or descending
+            else{
+                if(digits.length === 2){
+                    if(digits[1] +1 === current || digits[1] -1 === current)
+                        return true
+                    return false
+                }
+
+                return false
+            }
+        }
+        return false    
+    }
+
+    //check if the pin is unique
+    const isUnique = (pins:Array<string>, current:string) => {
+        if(pins.includes(current))
+            return false
+        return true
+    }
+
+    //convert int array to a string
+    const pinString = (digits:Array<number>) => {
+        let pinStr = ''
+        for(var i = 0; i<digits.length; i++){
+            pinStr += digits[i].toString()
+        }
+        return pinStr
+    }
+
+    return(
+        <div>
+            {pinCollection.pins.map((pin, i: number) => <p key={i}>{pin}</p>)}
+        </div>
+        
+    )
+}
+
+export default GeneratePins;
